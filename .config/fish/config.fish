b4737b8282fd8ea816cfef591b1e5ac304a78014
Muhammad Ali Faatikh Riziq <109046049+faatikhriziq@users.noreply.github.com>
2025-11-09 08:39:05 +0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 # Alias untuk bare repo dotfiles
 alias config 'git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 starship init fish | source
-source /usr/share/cachyos-fish-config/cachyos-config.fish
 fish_add_path -g /home/faatikhriziq/.config/herd-lite/bin
 fish_add_path -g -p /home/faatikhriziq/develop/flutter/bin
 export DOCKER_HOST=unix:///run/user/1000/podman/podman.sock
